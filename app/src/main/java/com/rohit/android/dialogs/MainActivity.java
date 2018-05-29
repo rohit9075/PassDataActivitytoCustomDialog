9bd2f70bc2236d291439b8a09a046fff260ff520
Rohit Kumar <rohit9075@gmail.com>
2018-05-29 17:59:25 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/android/dialogs/MainActivity.java
@@ -0,0 +1,37 @@
+package com.rohit.android.dialogs;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab == null) throw new AssertionError();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+
+        Person p = new Person("micky", "mouse", 35 );
+        // sending the person object p to the DataEntryDialog object
+        DataEntryDialog dialog = DataEntryDialog.newInstance(p);
+        dialog.setCancelable(false);
+        dialog.show(getSupportFragmentManager(), "DIALOG_FRAGMENT");
+    }
+
+}
